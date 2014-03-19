7932fd24127f5267ee0766dfaaa154d594105434
Natalia Alferova <nata@alfotos.com>
2014-03-19 22:21:09 +0400

    added a test class

---
+++ ANAddition.h
@@ -0,0 +1,13 @@
+//
+//  ANAddition.h
+//  gitTest
+//
+//  Created by Alferova Natalia on 19/03/14.
+//  Copyright (c) 2014 Alferova Natalia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ANAddition : NSObject
+
+@end
---
+++ ANAddition.m
@@ -0,0 +1,13 @@
+//
+//  ANAddition.m
+//  gitTest
+//
+//  Created by Alferova Natalia on 19/03/14.
+//  Copyright (c) 2014 Alferova Natalia. All rights reserved.
+//
+
+#import "ANAddition.h"
+
+@implementation ANAddition
+
+@end
